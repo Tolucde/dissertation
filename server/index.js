c9d172bcdddc524debaac6b64eec1dc5334d32f5
Philip <toluphilip619@gmail.com>
2025-01-23 09:50:47 +0000

    server timeout

--- server/index.js
+++ server/index.js
@@ -113,9 +113,10 @@ app.use((req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+server.timeout = 120000;
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (error) => {
